d87faad6b41aeaaba0030118cb796de300a0b1bf
Ayush Gupta <ayushgupta1371@gmail.com>
2026-05-27 21:11:37 +0530

    a

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,31 @@
-fun main() {
+import java.util.Scanner
+import kotlin.random.Random
 
-    for (n in 1..10) {
-        println("%2\$${n}s is %1\$${n}s".format( "Master", "Ayush"))
+fun main(){
+    val scanner = Scanner(System.`in`)
+    val random = Random
+
+    val a = scanner.nextInt()
+    val b = scanner.nextInt()
+    val c = scanner.nextInt()
+    val n = scanner.nextInt()
+
+    val generatedPassword = CharArray(n){'.'}
+
+    val lowerCase = mutableListOf<Char>()
+
+
+    for (i in 'a'..'z'){
+        lowerCase.add(i)
     }
 
+    for (i in 1..n){
+        val a = random.nextInt(0,n)
+        while (generatedPassword[a] == '.'){
+            generatedPassword[a] = lowerCase[random.nextInt(0, lowerCase.size)]
+        }
+    }
+
+
+    println(generatedPassword.joinToString(""))
 }
