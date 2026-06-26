2092798791ad5c7779efcf9c9051ddff02867f0a
Ayush Gupta <ayushgupta1371@gmail.com>
2026-06-26 17:33:01 +0530

    Capturing Groups session

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,23 @@
-fun main() {
-    val a = StringBuilder()
-    a.append("Ayush Gupta")
+fun main(){
+    val regex = "\\d+(\\.\\d+)?".toRegex()
+    val input = "The charger price is $10.99 and the headphone price $15.49 is on sale."
 
-    println(a.capacity())
+    val output = StringBuilder()
+    var lastIndex = 0
+    var result = regex.find(input)
+
+    while (result != null){
+        output.append(input.substring(lastIndex, result.range.first))
+
+        val amount = result.value.toDouble() * 1.1
+
+        output.append("%.2f".format(amount))
+
+        lastIndex = result.range.last + 1
+
+        result = result.next()
+    }
+    output.append(input.substring(lastIndex))
+
+    println(output)
 }
