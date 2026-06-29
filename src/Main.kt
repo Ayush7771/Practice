0fd91f933c2d78d85f0d7d8bc36d6814966249a5
Ayush Gupta <ayushgupta1371@gmail.com>
2026-06-29 17:44:55 +0530

    name validation practice

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,11 @@
 fun main() {
-    val regex = """(\d{4})-(\d{2})-(\d{2})""".toRegex()
-    val input = readln() //The event will happen on 2024-04-12.
+    val text = readln()
+    // write your code here
+    val regex = "([A-Z]+[a-z]*)(\\s[A-Z]+[a-z]*)?".toRegex()
+    val result = regex.findAll(text)
 
-    val matchResult = regex.find(input)
-
-    printGroups(matchResult)
-}
-
-fun printGroups(matchResult: MatchResult?) {
-    println(
-        """
-        Full match: ${matchResult?.groupValues[0]}
-        Year: ${matchResult?.groupValues[1]} 
-        Month: ${matchResult?.groupValues[2]}
-        Day: ${matchResult?.groupValues[3]}
-        """.trimIndent()
-    )
+    for (i in result){
+        println(i.value)
+    }
 
 }
