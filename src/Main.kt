074a8ad918294a51bd0816fd8bdf04bee7c304df
Ayush Gupta <ayushgupta1371@gmail.com>
2026-07-21 22:04:32 +0530

    developed compression algorithm

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,28 @@
-fun main(){
+fun main() {
     val input = readln()
-    val regex = Regex("[A-Z]+[a-z]+[0-9]+")
+    val splitedString = StringBuilder()
 
-    println(if(regex.matches(input)) "Password saved" else "Password is too simple")
+    for (i in input.indices) {
+        if (i == input.lastIndex) {
+            splitedString.append(input[i])
+            break
+        }
+
+        if (input[i] == input[i + 1]) {
+            splitedString.append(input[i])
+        } else {
+            splitedString.append(input[i])
+            splitedString.append("-")
+        }
+
+    }
+
+    val list = splitedString.split("-").toMutableList()
+    val output = StringBuilder()
+    for (i in list) {
+        output.append(i.first())
+        output.append(i.length)
+    }
+
+    println(output)
 }
