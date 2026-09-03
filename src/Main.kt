a20be01a7e1912ffb02affdab453be0e6000f07f
Ayush Gupta <ayushgupta1371@gmail.com>
2026-09-03 17:08:48 +0530

    OOP practice

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,25 @@
-fun main() {
-    val set = setOf(12, 34, 235, 2343, 1, 3, 4564, 23, 5, 6, 123545, 464)
+fun main(){
+    val account = BankAccount()
 
-    // Write your code here
-    val input = readln()
-    val inputNum = readln().toInt()
+    account.deposit(500)
+    account.deposit(200)
 
-    println(
-        when(input){
-            "first" -> set.firstOrNull{ it > inputNum}
-            "last" -> set.lastOrNull { it > inputNum }
-            else -> null
-        }
-    )
+    println(account.getBalance())
+
+}
+
+class BankAccount {
+    var balance = 1000
+
+    // Deposit ka member function banao
+    // amount receive kare
+    // balance ko increase kare
+    fun deposit(amount : Int){
+        balance += amount
+    }
+
+    // Balance return karne ka member function banao
+    fun getBalance(): Int{
+        return balance
+    }
 }
