d3f73f93817901b75ea59b41097625837824e6dc
Ayush Gupta <ayushgupta1371@gmail.com>
2026-09-09 17:32:38 +0530

    Learning polymorphism

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,19 @@
-fun main(){
-    val theDarkKnight = Comics(2, "Christian Bale", 1)
-    println(theDarkKnight.getInfo())
-    println(theDarkKnight.getIndiancost())
-}
+open class Vehicle {
+    open fun speed(age : Int) {
+        println("The vehicle has the speed")
+    }
 
-open class Book(val pages : Int, val author : String, val cost : Int){
-    fun getInfo() : String{
-        return "$pages $author $$cost"
+    fun speed(ayush : String){
+
     }
 }
 
-class Comics(pages: Int, author: String, cost: Int) : Book(pages, author, cost){
-    fun getIndiancost() : Int{
-        return cost * 95
+class Car : Vehicle() {
+    override fun speed(age: Int) {
+
+    }
+    fun speed(ready: Double){
+
     }
 }
+
