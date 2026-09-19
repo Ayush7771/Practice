5c25b9aed7788084e969d33419f0c4e4c6cf7f92
Ayush Gupta <ayushgupta1371@gmail.com>
2026-09-19 18:51:35 +0530

    Learning By Delegation

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,63 @@
-fun main() {
-    val zoo = Animal.getMethod()
-    val foo = Animal.getMethod()
+interface ICallbackReceiver {
+    fun onBeforeAction()
 
-    println(zoo === foo)
+    fun onAfterAction()
 
-}
-
-object Animal {
-    var instance: Animal? = null
-    fun getMethod(): Animal? {
-        if (instance == null) instance = Animal
-        return instance
+    fun action(ayush: () -> Unit) {
+        onBeforeAction()
+        ayush()
+        onAfterAction()
     }
 }
+
+interface ILogger {
+    fun getDateTime() = "05.11.2022-14:31:04"
+
+    val format: String
+        get() = "[${getDateTime()}]"
+
+    fun printMsg(s: String)
+}
+
+class BasicLogger : ILogger {
+    override fun printMsg(s: String) {
+        println(format + s)
+    }
+
+}
+
+class ConsoleNotifier(logger: ILogger) : ICallbackReceiver, ILogger by logger {
+
+    val onBeforeStr = "OnBefore!"
+    val onAfterStr = "OnAfter!"
+
+    override fun onBeforeAction() {
+        printMsg(onBeforeStr)
+    }
+
+    override fun onAfterAction() {
+        printMsg(onAfterStr)
+    }
+
+}
+
+class ExampleParser(notifier: ConsoleNotifier, logger: BasicLogger)
+    : ICallbackReceiver by notifier,
+    ILogger by logger {
+    fun start() = action { parseFiles() }
+
+    fun parseFiles() : Boolean{
+        printMsg("Parsing...")
+        return true
+    }
+}
+
+fun main(){
+    val basicLogger = BasicLogger()
+    val dateTimeNotifier = ConsoleNotifier(basicLogger)
+
+    val parser = ExampleParser(dateTimeNotifier, basicLogger)
+
+    parser.start()
+}
+
