bb6e2c72291d315e003ef917272d5860a459791a
Ayush Gupta <ayushgupta1371@gmail.com>
2026-09-20 14:22:09 +0530

    Understanding Diamond Problem

--- src/Main.kt
+++ src/Main.kt
@@ -1,63 +1,23 @@
-interface ICallbackReceiver {
-    fun onBeforeAction()
-
-    fun onAfterAction()
-
-    fun action(ayush: () -> Unit) {
-        onBeforeAction()
-        ayush()
-        onAfterAction()
+interface Animal{
+    fun makeSound(){
+        println("Animal")
     }
 }
 
-interface ILogger {
-    fun getDateTime() = "05.11.2022-14:31:04"
-
-    val format: String
-        get() = "[${getDateTime()}]"
-
-    fun printMsg(s: String)
+interface Bird{
+    fun makeSound(){
+        println("Bird")
+    }
 }
 
-class BasicLogger : ILogger {
-    override fun printMsg(s: String) {
-        println(format + s)
-    }
-
-}
-
-class ConsoleNotifier(logger: ILogger) : ICallbackReceiver, ILogger by logger {
-
-    val onBeforeStr = "OnBefore!"
-    val onAfterStr = "OnAfter!"
-
-    override fun onBeforeAction() {
-        printMsg(onBeforeStr)
-    }
-
-    override fun onAfterAction() {
-        printMsg(onAfterStr)
-    }
-
-}
-
-class ExampleParser(notifier: ConsoleNotifier, logger: BasicLogger)
-    : ICallbackReceiver by notifier,
-    ILogger by logger {
-    fun start() = action { parseFiles() }
-
-    fun parseFiles() : Boolean{
-        printMsg("Parsing...")
-        return true
+class NonHumans : Animal, Bird{
+    override fun makeSound(){
+        super<Bird>.makeSound()
+        super<Animal>.makeSound()
     }
 }
 
 fun main(){
-    val basicLogger = BasicLogger()
-    val dateTimeNotifier = ConsoleNotifier(basicLogger)
-
-    val parser = ExampleParser(dateTimeNotifier, basicLogger)
-
-    parser.start()
+    val nonHumans = NonHumans()
+    nonHumans.makeSound()
 }
-
