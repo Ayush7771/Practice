5306c005c98c805e422f610fdf6defbf5590e773
Ayush Gupta <ayushgupta1371@gmail.com>
2026-09-21 16:09:06 +0530

    Learning Vetoable Property Delegation

--- src/Main.kt
+++ src/Main.kt
@@ -3,12 +3,14 @@ import kotlin.properties.Delegates
 fun main(){
     val user = User()
     println(user.name)
+    user.name = "Papa"
+    println(user.name)
     user.name = "Khushi"
     println(user.name)
 }
 
 class User {
-    var name: String by Delegates.observable("Ayush"){property, oldValue, newValue ->
-        println("${property.name}: $oldValue -> $newValue")
+    var name: String by Delegates.vetoable("Ayush"){property, oldValue, newValue ->
+        newValue == "Khushi"
     }
 }
