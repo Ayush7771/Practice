6ea12c11b5cb15a5da7c482e7898b41c8752c403
Ayush Gupta <ayushgupta1371@gmail.com>
2026-09-21 13:22:07 +0530

    Learning Observable Property Delegation

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,14 @@
+import kotlin.properties.Delegates
+
 fun main(){
-    val lazyValue : String by lazy {
-        println("first")
-        "second"
+    val user = User()
+    println(user.name)
+    user.name = "Khushi"
+    println(user.name)
+}
+
+class User {
+    var name: String by Delegates.observable("Ayush"){property, oldValue, newValue ->
+        println("${property.name}: $oldValue -> $newValue")
     }
-    println(lazyValue)
-    println(lazyValue)
 }
