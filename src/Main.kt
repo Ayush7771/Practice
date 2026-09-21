eeafdeb7d7174a662a300d1e2946269fc13c3267
Ayush Gupta <ayushgupta1371@gmail.com>
2026-09-21 16:21:37 +0530

    Adding property data to map

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,15 @@
 import kotlin.properties.Delegates
 
 fun main(){
-    val user = User()
+    val user = User(mutableMapOf(
+        "name" to "Ayush",
+        "age" to 22
+    ))
     println(user.name)
-    user.name = "Papa"
-    println(user.name)
-    user.name = "Khushi"
-    println(user.name)
+    println(user.age)
 }
 
-class User {
-    var name: String by Delegates.vetoable("Ayush"){property, oldValue, newValue ->
-        newValue == "Khushi"
-    }
+class User(val map : MutableMap<String, Any?>) {
+    var name: String by map
+    var age : Int by map
 }
