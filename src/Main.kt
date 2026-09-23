f720558608e8efb2effded20bd9e0680d8edb877
Ayush Gupta <ayushgupta1371@gmail.com>
2026-09-23 18:05:51 +0530

    Learning nested and Inner Classes

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,9 @@
-import kotlin.properties.Delegates
+class Shape{
+    class Square(){
 
-fun main(){
-    val user = User(mutableMapOf(
-        "name" to "Ayush",
-        "age" to 22
-    ))
-    println(user.name)
-    println(user.age)
-}
+    }
 
-class User(val map : MutableMap<String, Any?>) {
-    var name: String by map
-    var age : Int by map
+    inner class Rectangle(){
+
+    }
 }
