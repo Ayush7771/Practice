efefcc49bfea2249915ebc10b5b990fe4ad4da6d
Ayush Gupta <ayushgupta1371@gmail.com>
2026-09-24 12:55:07 +0530

    Updated version of Flood Fill Algorithm

--- src/Main.kt
+++ src/Main.kt
@@ -1,53 +1,49 @@
-fun main(args: Array<String>) {
-    val img = arrayOf<IntArray>(
-        intArrayOf(1, 1, 1, 0),
-        intArrayOf(0, 1, 1, 1),
-        intArrayOf(1, 1, 1, 1)
+fun main() {
+    val grid = mutableListOf(
+        mutableListOf(1,1,1,0),
+        mutableListOf(0,1,1,0),
+        mutableListOf(1,0,1,1)
     )
 
     val sr = 1
     val sc = 2
     val newColor = 2
 
-    val result = floodFill(img, sr, sc, newColor)
+    val result = floodFill(grid, sr, sc, newColor)
 
     for (row in result) {
         for (pixel in row) {
-            print(pixel.toString() + " ")
+            print("$pixel ")
         }
         println()
     }
 }
 
-fun floodFill(img: Array<IntArray>, sr: Int, sc: Int, newColor: Int): Array<IntArray> {
-    // If the starting pixel already has the new color,
-    // no changes are needed
+fun floodFill(grid: MutableList<MutableList<Int>>, sr: Int, sc: Int, newColor: Int): MutableList<MutableList<Int>> {
 
-    if (img[sr][sc] == newColor) {
-        return img
+    if (grid[sr][sc] == newColor) {
+        return grid
     }
 
-    // Call DFS to start filling from the source pixel
-    // Store original color
-    val oldColor = img[sr][sc]
-    dfs(img, sr, sc, oldColor, newColor)
+    val oldColor = grid[sr][sc]
+    checkNear(grid, sr, sc, oldColor, newColor)
 
-    return img
+    return grid
 }
 
-fun dfs(img: Array<IntArray>, x: Int, y: Int, oldColor: Int, newColor: Int) {
-    if (x < 0 || x >= img.size || y < 0 || y >= img[0].size || img[x][y] != oldColor) {
+fun checkNear(grid: MutableList<MutableList<Int>>, x: Int, y: Int, oldColor: Int, newColor: Int) {
+    if (x < 0 || x >= grid.size || y < 0 || y >= grid[0].size || grid[x][y] != oldColor) {
         return
     }
-
-    // Update the color of the current pixel
-    img[x][y] = newColor
-
-    // Recursively visit all 4 connected neighbors
-    dfs(img, x + 1, y, oldColor, newColor)
-    dfs(img, x - 1, y, oldColor, newColor)
-    dfs(img, x, y + 1, oldColor, newColor)
-    dfs(img, x, y - 1, oldColor, newColor)
+    
+    grid[x][y] = newColor
+    
+    checkNear(grid, x + 1, y, oldColor, newColor)
+    checkNear(grid, x - 1, y, oldColor, newColor)
+    checkNear(grid, x, y + 1, oldColor, newColor)
+    checkNear(grid, x, y - 1, oldColor, newColor)
+    checkNear(grid, x + 1, y - 1, oldColor, newColor)
+    checkNear(grid, x - 1, y - 1, oldColor, newColor)
+    checkNear(grid, x + 1, y + 1, oldColor, newColor)
+    checkNear(grid, x - 1, y + 1, oldColor, newColor)
 }
-
-
